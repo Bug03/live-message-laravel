954fb310b64391119790f74479b3fd3ff2ae5afa
Bug03 <90082574+Bug03@users.noreply.github.com>
2024-07-10 15:53:21 +0700

    Implement real-time notifications using Pusher: - Add Pusher library and configure it in the project - Implement server-side authentication for Pusher channels - Set up client-side Pusher connection and event handling - Add real-time notification feature for user activities

---
+++ app/Events/Message.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Message implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('message.'.$this->message->to_id),
+        ];
+    }
+
+    function broadcastWith(): array {
+        return [
+            'id' => $this->message->id,
+            'content' => $this->message->content,
+            'to_id' => $this->message->to_id,
+            'attachment' => json_decode($this->message->attachment),
+            'from_id' => auth()->user()->id
+        ];
+    }
+}
--- resources/views/messenger/components/message-card.blade.php
+++ resources/views/messenger/components/message-card.blade.php
@@ -13,7 +13,7 @@
         <span class="time"> {{ timeAgo($message->created_at) }}</span>
         @if ($message->from_id === auth()->user()->id)
             <a class="action dlt-message" data-id="{{ $message->id }}" href=""><i class="fas fa-trash"></i></a>
-        @endif)
+        @endif
 
     </div>
 </div>
